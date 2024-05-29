05cb1ec432fda7082a07bb297d682945ff9181ad
Tyrion Lannister <win.pomerantz@gmail.com>
2024-05-29 14:37:37 -0700

    updated site color scheme

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -6,16 +6,18 @@ function AboutPage() {
         <h1>About Me</h1>
         <img src="../../assets/images/win-headshot-circle.png" alt="Win Pomerantz Headshot"
             className="headshot" />
-        <h2>Software Engineer, Marketer, Lifelong Student</h2>
-        <p className="centered">
-            I'm Win Pomerantz, a marketer turned aspiring web developer. With over seven years
-            of experience in brand and digital marketing at companies like Charles Schwab, Ubisoft, and eBay, I've
-            developed a strong understanding of the customer, enabling me to better craft engaging narratives and user experiences.
-            With a bachelors degree from Emory University and MBA from UC Berkeley's Haas School of Business,
-            I'm currently diving into the world of coding through the full-time UC Berkeley Coding Bootcamp.
-            I'm passionate about blending creativity with technical skills to build captivating web experiences.
-        </p>
-        <p className="centered"> Join me as I transition from marketing to coding and explore the exciting intersection of art and technology! </p>
+        <section className="about-text">
+            <h2>Software Engineer, Marketer, Lifelong Student</h2>
+            <p className="centered">
+                I'm Win Pomerantz, a marketer turned aspiring web developer. With over seven years
+                of experience in brand and digital marketing at companies like Charles Schwab, Ubisoft, and eBay, I've
+                developed a strong understanding of the customer, enabling me to better craft engaging narratives and user experiences.
+                With a bachelors degree from Emory University and MBA from UC Berkeley's Haas School of Business,
+                I'm currently diving into the world of coding through the full-time UC Berkeley Coding Bootcamp.
+                I'm passionate about blending creativity with technical skills to build captivating web experiences.
+            </p>
+            <p className="centered"> Join me as I transition from marketing to coding and explore the exciting intersection of art and technology! </p>
+        </section>
     </section>
 }
 
